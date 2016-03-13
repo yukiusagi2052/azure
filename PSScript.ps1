addaed55e8fa2673e6e19477184fd1ade946f8e0
yukiusagi2052 <yukiusagi2052@gmail.com>
2016-03-13 12:01:24 +0900

    update

---
+++ PSScript.ps1
@@ -0,0 +1,13 @@
+﻿#
+# PSScript.ps1
+#
+
+New-Item -Path 'C:\Powershell' -ItemType directory > $null
+
+Write-Output "Hello Powershell Script" | Set-Content 'C:\Powershell\Hello.txt'
+
+Write-Output $PWD | Set-Content 'C:\Powershell\PWD.txt'
+
+Write-Output $env:USERNAME | Set-Content 'C:\Powershell\UserName.txt'
+
+Write-Output $env:USERDOMAIN | Set-Content 'C:\Powershell\UserDomain.txt'
