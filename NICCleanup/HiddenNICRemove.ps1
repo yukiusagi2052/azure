3143d2f345697769013eccf72472d083442c8541
yukiusagi2052 <yukiusagi2052@gmail.com>
2016-03-22 01:09:40 +0900

    update NICCleanup

--- NICCleanup/HiddenNICRemove.ps1
+++ NICCleanup/HiddenNICRemove.ps1
@@ -1,4 +1,4 @@
-Import-Module .\Release\DeviceManagement.psd1
+Import-Module .\DeviceManagement\DeviceManagement.psd1
 $hiddenHypVNics = Get-Device -ControlOptions DIGCF_ALLCLASSES | Sort-Object -Property Name | Where-Object { ($_.IsPresent -eq $false) -and ($_.Name -like "Microsoft Hyper-V Network Adapter*") }
 $hiddenHypVNics | ForEach-Object {
   .\devcon.exe remove "@$($_.InstanceId.Replace("`0", ''))"
--- NICCleanup/Setup.ps1
+++ NICCleanup/Setup.ps1
@@ -4,6 +4,7 @@ $TargetDir = 'C:\Packages\NICCleanup'
 New-Item -Path $TargetDir -Type Directory
 Set-Location $TargetDir
 
+## Step1 devcon.exe
 $url = 'http://go.microsoft.com/?linkid=9829438'
 $uri = New-Object System.Uri($url)
 $file = 'MicrosoftEasyFix25010.mini.diagcab'
@@ -11,7 +12,10 @@ $cli = New-Object System.Net.WebClient
 $cli.DownloadFile($uri, (Join-Path $TargetDir $file))
 
 Start-Process -FilePath expand.exe -ArgumentList "MicrosoftEasyFix25010.mini.diagcab . -f:devcon_V8_AMD64.exe" -Wait
+Rename-Item -Path .\devcon_V8_AMD64.exe devcon.exe
+Remove-Item -Path .\MicrosoftEasyFix25010.mini.diagcab
 
+## Step2 Device Management Powershell
 $url = 'https://gallery.technet.microsoft.com/scriptcenter/Device-Management-7fad2388/file/65051/2/DeviceManagement.zip'
 $uri = New-Object System.Uri($url)
 $file = 'DeviceManagement.zip'
@@ -32,17 +36,21 @@ Configuration DscUnzip
 }
 DscUnzip -OutputPath .
 Start-DscConfiguration .\DscUnzip -Wait -Verbose
+Remove-Item -Recurse -Path .\DscUnzip
+Remove-Item -Path .\DeviceManagement.zip
+Rename-Item .\Release DeviceManagement
 
-
-$url = 'https://raw.githubusercontent.com/yukiusagi2052/azure/master/NICCleanup/Setup-NICCleanup.ps1'
-$uri = New-Object System.Uri($url)
-$file = Split-Path $uri.AbsolutePath -Leaf
-$cli = New-Object System.Net.WebClient
-$cli.DownloadFile($uri, (Join-Path $TargetDir $file))
-
-
+## Step3 HiddenNICRemove.ps1
 $url = 'https://raw.githubusercontent.com/yukiusagi2052/azure/master/NICCleanup/HiddenNICRemove.ps1'
 $uri = New-Object System.Uri($url)
 $file = Split-Path $uri.AbsolutePath -Leaf
 $cli = New-Object System.Net.WebClient
 $cli.DownloadFile($uri, (Join-Path $TargetDir $file))
+
+## Step4 NICCleanup.bat
+$url = 'https://raw.githubusercontent.com/yukiusagi2052/azure/master/NICCleanup/NICCleanup.bat'
+$uri = New-Object System.Uri($url)
+$file = Split-Path $uri.AbsolutePath -Leaf
+$cli = New-Object System.Net.WebClient
+$cli.DownloadFile($uri, (Join-Path $TargetDir $file))
+
